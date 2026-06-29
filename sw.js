c1ea4e870f8f2c93a648ebf4527b70848653ac04
babarzonecom-web <babarzone.com@gmail.com>
2026-06-29 16:45:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION='v473.32';
+const SW_VERSION='v473.34';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(cs=>cs.forEach(c=>{c.postMessage({type:'SW_UPDATED',version:SW_VERSION});c.navigate(c.url);})));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
